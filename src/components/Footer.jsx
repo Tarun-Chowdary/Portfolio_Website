7ae8594b55c042609e5848c0d45dbd0718d8d3db
lahari venkatesh yegi <sci.tarun2992@gmail.com>
2026-05-20 18:18:33 +0530

    updated social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
   const socialLinks = [
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/taryegi/' },
     { name: 'Github', url: 'https://github.com/Tarun-Chowdary' },
-    { name: 'Resume', url: 'https://drive.google.com/file/d/1Vg9v9rXuOcmgx7z1vqqOjJKWG7V3_FW-/view?usp=sharing' },
+    { name: 'Resume', url: 'https://drive.google.com/file/d/1BzCDFBGttqCrmVMkxIanax-qS1doBalw/view?usp=sharing' },
   ];
 
   return (
